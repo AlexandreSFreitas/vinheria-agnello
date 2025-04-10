ebf5d373821b7520aae2efe82d436c77b11ea24a
Henry Cortesi <henry.cortesi2007@gmail.com>
2025-04-10 10:30:40 -0300

    Solicitando o tipo de vinho ao usuário.

--- vinheria-agnello-main/src/js/script.js
+++ vinheria-agnello-main/src/js/script.js
@@ -7,3 +7,18 @@ if (nomeDoVinho == "") {
 }
 //Cadastrando o nome do vinho no console.
 console.log("Nome do vinho:", nomeDoVinho)
+
+//Solicitando o tipo de vinho ao usuário. 
+var tipoDeVinho = prompt("Insira o tipo de vinho: tinto, branco ou rosé")
+//Caso o usuário não insira o tipo de vinho, solicite novamente.
+if (tipoDeVinho == "") {
+    alert("O tipo de vinho não foi inserido. Insira novamente.")
+    var tipoDeVinho = prompt("Insira o tipo de vinho: tinto, branco ou rosé")
+}
+//Caso o usuário não insira um dos três tipos de vinho, solicite novamente.
+else if (tipoDeVinho != "tinto" || tipoDeVinho != "branco"|| tipoDeVinho != "rosé"|| tipoDeVinho != "Tinto"|| tipoDeVinho || "Branco"|| tipoDeVinho != "Rosé" ){
+    alert("Tipo não encontrado. Insira um tipo válido.")
+    var tipoDeVinho = prompt("Insira o tipo de vinho: tinto, branco ou rosé")
+//Cadastrando o tipo de vinho no console.
+}
+console.log("Tipo de vinho:", tipoDeVinho)
